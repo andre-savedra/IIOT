c007d1786b4f89c782cc7385576ec19cb2de06a9
andre-savedra <andre_savedra@hotmail.com>
2025-02-03 21:42:31 -0300

    first class

---
+++ TURMA_A/PROJETO_1/main.ino
@@ -0,0 +1,25 @@
+#define BUTTON_PIN 2
+#define LED_PIN 1
+
+bool button = 0, led = 0;
+
+//configurações:
+void setup(){
+    pinMode(BUTTON_PIN,INPUT_PULLUP);
+    pinMode(LED_PIN,OUTPUT);
+}
+
+//lógica:
+void loop(){
+    /*if(!button){
+        led = 1;
+    }
+    else{
+        led = 0;
+    }*/
+    led = !button;
+
+    //I-O's:
+    button = digitalRead(BUTTON_PIN);
+    digitalWrite(LED_PIN,led);
+}
