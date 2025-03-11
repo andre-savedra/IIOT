316e9874c176c70d2f5481b31e2c5a7de2ef2866
Desenvolvimento <dsadm@educ501.local>
2025-03-10 22:19:01 -0300

    aula 10-03-25

--- TURMA_A/DOMOTICA/domotica/src/views/Devices.vue
+++ TURMA_A/DOMOTICA/domotica/src/views/Devices.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Device } from '@/models/devices';
+import { Device, Environment } from '@/models/devices';
 import { ref, reactive } from 'vue';
 
 const ar: Device = reactive(new Device());
@@ -16,20 +16,44 @@ iluminacao.name = 'Lâmpada Led';
 iluminacao.state = true;
 iluminacao.icon = 'light';
 
+const sala: Environment = reactive(new Environment());
+sala.name = 'Sala de Estar';
+sala.devices = [ ar, tv, iluminacao ];
+
+
+const tomada: Device = reactive(new Device());
+tomada.name = 'Tomada inteligente';
+tomada.state = false;
+tomada.icon = 'power';
+
+const quarto: Environment = reactive(new Environment());
+quarto.name = 'Quarto de Hóspedes';
+quarto.devices = [ tomada ];
+
+const environments: Array<Environment> = reactive([]);
+environments.push(sala);
+environments.push(quarto);
+
 
 </script>
 
 <template>
     <h1>Seus Dispositivos: </h1>
-    
-    <div>        
-        <h5>{{ ar.name }}</h5>
-        <span class="icons material-icons-round">{{ ar.icon }}</span>
-        <div :class="`button-${ar.state}`">
-            <button class="on">ON</button>
-            <button class="off">OFF</button>
-        </div>      
+
+    <div v-for="(currentEnvironment, envId) in environments" :key="envId">
+        <h3>{{ currentEnvironment.name }}</h3>
+        <div v-for="(currentDevice, id) in currentEnvironment.devices" :key="id">        
+            <h5>{{ currentDevice.name }}</h5>
+            <span class="icons material-icons-round">{{ currentDevice.icon }}</span>
+            <div :class="`button-${currentDevice.state}`">
+                <button class="on">ON</button>
+                <button class="off">OFF</button>
+            </div>      
+        </div>    
+        <hr>
     </div>
+    
+    
     
 </template>
 
