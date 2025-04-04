ef7fd066ee3edd3b32092c4214e58ed5772405c6
Desenvolvimento <dsadm@educ501.local>
2025-04-03 22:24:24 -0300

    03-04-25

---
+++ TURMA_B/DOMOTICA/domotica/src/services/services.config.ts
@@ -0,0 +1,10 @@
+import axios, {type AxiosResponse } from "axios";
+export const CONTENTFUL_CDN_URL = 'https://cdn.contentful.com';
+export const CONTENTFUL_API_URL = 'https://api.contentful.com';
+
+
+export const getCdnAxios = ()=> {
+    const createdAxios = axios.create({
+
+    });
+}
